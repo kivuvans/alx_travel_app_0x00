69b38dd049942bac511f07296dc7235daf681851
kivuvans <35780006+kivuvans@users.noreply.github.com>
2025-11-18 21:20:47 +0300

    Create serializers.py

---
+++ alx_travel_app/listings/serializers.py
@@ -0,0 +1,22 @@
+from rest_framework import serializers
+from .models import Listing, Booking, Review
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = "__all__"
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
+        fields = "__all__"
+
+
+class ListingSerializer(serializers.ModelSerializer):
+    bookings = BookingSerializer(many=True, read_only=True)
+    reviews = ReviewSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Listing
+        fields = "__all__"
